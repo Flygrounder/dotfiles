b4cfd0f2f2c92e54cbb02affbbcb3f9fc9a75ef6
Artyom Belousov <flygrounder@yandex.ru>
2022-07-17 22:49:16 +0300

    Add battery check module

--- nixos/users/flygrounder/laptop.nix
+++ nixos/users/flygrounder/laptop.nix
@@ -2,39 +2,11 @@
 {
   imports = [
     ./generic.nix
+    ../modules/batteryCheck.nix
   ];
 
-  modules.xmobar.template =   " <fn=1>  </fn> %UnsafeXMonadLog% }{ <fn=2></fn> %kbd%    %brightness%    %battery%    %volume%    <fn=2></fn> %date%    %trayerpad%";
-
-  systemd.user = {
-    services = {
-      batteryCheck = {
-        Unit = {
-          Description = "Alerts user if battery level is low";
-        };
-
-        Service = {
-          Type = "simple";
-          ExecStart = "/home/flygrounder/.local/share/scripts/battery.py alert";
-          Environment = "PATH=${pkgs.python3}/bin:${pkgs.dunst}/bin:${pkgs.acpi}/bin";
-        };
-      };
-    };
-    timers = {
-      batteryCheck = {
-        Unit = {
-          Description = "Runs battery check every 10 minutes";
-        };
-
-        Timer = {
-          OnBootSec = "10min";
-          OnUnitActiveSec = "10min";
-        };
-
-        Install = {
-          WantedBy = [ "timers.target" ];
-        };
-      };
-    };
+  modules = {
+    batteryCheck.enable = true;
+    xmobar.template =   " <fn=1>  </fn> %UnsafeXMonadLog% }{ <fn=2></fn> %kbd%    %brightness%    %battery%    %volume%    <fn=2></fn> %date%    %trayerpad%";
   };
 }
---
+++ nixos/users/modules/batteryCheck.nix
@@ -0,0 +1,48 @@
+{ pkgs, lib, config, ... }:
+with lib;
+let
+  cfg = config.modules.batteryCheck;
+in
+{
+  options.modules.batteryCheck = {
+    enable = mkOption {
+      default = false;
+      type = types.bool;
+      description = "Enable battery check daemon";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.user = {
+      services = {
+        batteryCheck = {
+          Unit = {
+            Description = "Alerts user if battery level is low";
+          };
+
+          Service = {
+            Type = "simple";
+            ExecStart = "/home/flygrounder/.local/share/scripts/battery.py alert";
+            Environment = "PATH=${pkgs.python3}/bin:${pkgs.dunst}/bin:${pkgs.acpi}/bin";
+          };
+        };
+      };
+      timers = {
+        batteryCheck = {
+          Unit = {
+            Description = "Runs battery check every 10 minutes";
+          };
+
+          Timer = {
+            OnBootSec = "10min";
+            OnUnitActiveSec = "10min";
+          };
+
+          Install = {
+            WantedBy = [ "timers.target" ];
+          };
+        };
+      };
+    };
+  };
+}
