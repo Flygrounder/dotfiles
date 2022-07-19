585596a7959a1b170d8713c630143d3c33b4f88e
Artyom Belousov <flygrounder@yandex.ru>
2022-07-19 08:35:26 +0300

    tre -> tree, opacity 1 in rofi

--- modules/system/shell.nix
+++ modules/system/shell.nix
@@ -27,7 +27,7 @@ in
         fd
         neofetch
         ripgrep
-        tre-command
+        tree
         unzip
       ];
 
@@ -44,7 +44,6 @@ in
           };
           functions = {
             fish_greeting = "";
-            tre = "command tre $argv -e; and source /tmp/tre_aliases_$USER ^/dev/null";
           };
           interactiveShellInit = ''fish_vi_key_bindings
 direnv hook fish | source
