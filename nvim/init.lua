a92b939bcfd3a885392340a9bd4528595c9a811e
Artyom Belousov <flygrounder@mail.ru>
2025-07-31 17:48:00 +0300

    Update neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,16 +5,19 @@ vim.o.number = true
 vim.o.swapfile = false
 vim.g.mapleader = ' '
 
-vim.pack.add{
-  { src = 'https://github.com/neovim/nvim-lspconfig' },
-  { src = 'https://github.com/echasnovski/mini.deps' },
-  { src = 'https://github.com/catppuccin/nvim' },
-  { src = 'https://github.com/mason-org/mason.nvim' },
-  { src = 'https://github.com/stevearc/oil.nvim' },
-  { src = 'https://github.com/nvim-lua/plenary.nvim' },
-  { src = 'https://github.com/nvim-telescope/telescope.nvim' },
-  { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
-  { src = 'https://github.com/nvim-lualine/lualine.nvim' },
+vim.pack.add {
+	{ src = 'https://github.com/neovim/nvim-lspconfig' },
+	{ src = 'https://github.com/echasnovski/mini.deps' },
+	{ src = 'https://github.com/catppuccin/nvim' },
+	{ src = 'https://github.com/mason-org/mason.nvim' },
+	{ src = 'https://github.com/stevearc/oil.nvim' },
+	{ src = 'https://github.com/nvim-lua/plenary.nvim' },
+	{ src = 'https://github.com/nvim-telescope/telescope.nvim' },
+	{ src = 'https://github.com/nvim-tree/nvim-web-devicons' },
+	{ src = 'https://github.com/nvim-lualine/lualine.nvim' },
+	{ src = 'https://github.com/coffebar/neovim-project' },
+	{ src = 'https://github.com/Shatur/neovim-session-manager' },
+	{ src = 'https://github.com/stevearc/conform.nvim' },
 }
 
 require('mini.completion').setup()
@@ -26,20 +29,36 @@ require('mason').setup()
 require('oil').setup()
 require('nvim-web-devicons').setup()
 require('lualine').setup()
+require('neovim-project').setup({
+	projects = {
+		'~/Projects/*',
+		'~/.config/*',
+	},
+	picker = {
+		type = 'telescope',
+	}
+})
+require('conform').setup({
+	format_on_save = {
+		lsp_format = "fallback",
+		timeout_ms = 500,
+	},
+})
 
 local telescope = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>f', telescope.find_files, {})
-vim.keymap.set('n', '<leader>F', function() telescope.find_files({ no_ignore = true }) end, {})
+vim.keymap.set('n', '<leader>f', function() telescope.find_files({ hidden = true }) end, {})
+vim.keymap.set('n', '<leader>F', function() telescope.find_files({ hidden = true, no_ignore = true }) end, {})
 vim.keymap.set('n', '<leader>g', telescope.live_grep, {})
 vim.keymap.set('n', '<leader>h', telescope.help_tags, {})
 vim.keymap.set('n', '<leader>d', telescope.diagnostics, {})
 vim.keymap.set('n', '<leader>i', vim.lsp.buf.format, {})
 vim.keymap.set('n', '<leader>o', '<CMD>Oil<CR>', {})
 vim.keymap.set('n', '<leader>m', '<CMD>Mason<CR>', {})
+vim.keymap.set('n', '<leader>p', '<CMD>NeovimProjectHistory<CR>', {})
 vim.keymap.set('n', 'gd', telescope.lsp_definitions, {})
 vim.keymap.set('n', 'gD', telescope.lsp_references, {})
 
-vim.lsp.enable({ 'gopls', 'pyright' })
+vim.lsp.enable({ 'gopls', 'pyright', 'lua_ls' })
 vim.cmd.colorscheme('catppuccin')
 vim.diagnostic.config({ virtual_text = true })
