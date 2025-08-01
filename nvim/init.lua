4767b3307c4c8268909079945ca270369d0a1d13
flygrounder <flygrounder@mail.ru>
2025-08-01 08:03:31 +0300

    Add harpoon

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,8 +19,36 @@ vim.pack.add {
 	{ src = 'https://github.com/coffebar/neovim-project' },
 	{ src = 'https://github.com/Shatur/neovim-session-manager' },
 	{ src = 'https://github.com/stevearc/conform.nvim' },
-	{ src = 'https://github.com/lewis6991/gitsigns.nvim' }
+	{ src = 'https://github.com/lewis6991/gitsigns.nvim' },
+	{ src = 'https://github.com/ThePrimeagen/harpoon',                   version = 'harpoon2' },
+	{ src = 'https://github.com/nvim-telescope/telescope-ui-select.nvim' }
 }
+
+local harpoon = require("harpoon")
+
+harpoon:setup({
+	settings = {
+		save_on_toggle = true,
+		sync_on_ui_close = true,
+	}
+})
+
+local config_group = vim.api.nvim_create_augroup("SessionGroup", {})
+vim.api.nvim_create_autocmd({ "User" }, {
+	pattern = "SessionLoadPost",
+	group = config_group,
+	callback = function()
+		require("harpoon")
+	end,
+})
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>j", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<M-q>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<M-w>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<M-e>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<M-r>", function() harpoon:list():select(4) end)
 
 require('mini.completion').setup()
 require('mini.pairs').setup()
@@ -53,6 +81,7 @@ require('telescope').setup {
 		file_ignore_patterns = { '^.git/' }
 	}
 }
+require('telescope').load_extension('ui-select')
 
 local telescope = require('telescope.builtin')
 
@@ -64,6 +93,7 @@ vim.keymap.set('n', '<leader>G', function() telescope.live_grep({ additional_arg
 vim.keymap.set('n', '<leader>b', '<CMD>Gitsigns blame<CR>', {})
 vim.keymap.set('n', '<leader>h', telescope.help_tags, {})
 vim.keymap.set('n', '<leader>d', telescope.diagnostics, {})
+vim.keymap.set('n', '<leader>w', telescope.lsp_workspace_symbols, {})
 vim.keymap.set('n', '<leader>i', vim.lsp.buf.format, {})
 vim.keymap.set('n', '<leader>o', '<CMD>Oil<CR>', {})
 vim.keymap.set('n', '<leader>m', '<CMD>Mason<CR>', {})
